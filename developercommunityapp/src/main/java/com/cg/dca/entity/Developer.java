1892d30d4e107e87071ec454f3aa2a3a34c79af4
Azmaths <47237313+Azmaths@users.noreply.github.com>
2021-08-05 10:13:43 +0530

    Added Developer Entity Class

--- developercommunityapp/src/main/java/com/cg/dca/entity/Developer.java
+++ developercommunityapp/src/main/java/com/cg/dca/entity/Developer.java
@@ -1,18 +1,128 @@
 package com.cg.dca.entity;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 
-public class Developer {
-	private int devId;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Developer")
+public class Developer implements Serializable {
+
+	@Column(name="dev_id")
+	@Id
+	private	int devId;
+	@Column(name="name")
 	private String name;
+	@Column(name="email")
 	private String email;
-	private String skillLevel;	// Beginner/Intermediate/Expert
+	@Column(name="skill_level")
+	private String skillLevel;
+	@Column(name="member_since")
 	private LocalDate memberSince;
-	private List<Feed> feeds;
+	@Column(name="total_feeds")
+	private int  totalFeeds;
+	@Column(name="reputation")
+	private int  reputations;
+
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="fk_user_id")
 	private User user;
-	private int totalFeeds;	
-	private int reputation;		// Likes on Feed/Response by developer increase reputation
-	private boolean isVerified;
-	private boolean isBlocked;	// Can be blocked by admin on the ground of unethical feed/response
+
+	
+	@OneToMany(mappedBy="developer",cascade=CascadeType.ALL)
+	private  List<Feed> feedList;
+
+	@Column(name="is_verified")
+	private Boolean isVerified;
+	@Column(name="is_blocked")
+	private Boolean isBlocked;
+	
+	
+	
+	public int getDevId() {
+		return devId;
+	}
+	public void setDevId(int devId) {
+		this.devId = devId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSkillLevel() {
+		return skillLevel;
+	}
+	public void setSkillLevel(String skillLevel) {
+		this.skillLevel = skillLevel;
+	}
+	public LocalDate getMemberSince() {
+		return memberSince;
+	}
+	public void setMemberSince(LocalDate memberSince) {
+		this.memberSince = memberSince;
+	}
+	public int getTotalFeeds() {
+		return totalFeeds;
+	}
+	public void setTotalFeeds(int totalFeeds) {
+		this.totalFeeds = totalFeeds;
+	}
+	public int getReputations() {
+		return reputations;
+	}
+	public void setReputations(int reputations) {
+		this.reputations = reputations;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public List<Feed> getFeedList() {
+		return feedList;
+	}
+	public void setFeedList(List<Feed> feedList) {
+		this.feedList = feedList;
+	}
+	public Boolean getIsVerified() {
+		return isVerified;
+	}
+	public void setIsVerified(Boolean isVerified) {
+		this.isVerified = isVerified;
+	}
+	public Boolean getIsBlocked() {
+		return isBlocked;
+	}
+	public void setIsBlocked(Boolean isBlocked) {
+		this.isBlocked = isBlocked;
+	}
+	@Override
+	public String toString() {
+		return "Developer [devId=" + devId + ", name=" + name + ", email=" + email + ", skillLevel=" + skillLevel
+				+ ", memberSince=" + memberSince + ", totalFeeds=" + totalFeeds + ", reputations=" + reputations
+				+ ", user=" + user + ", feedList=" + feedList + ", isVerified=" + isVerified + ", isBlocked="
+				+ isBlocked + "]";
+	}
+
+
+
 }
