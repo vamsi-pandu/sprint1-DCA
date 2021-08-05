d30eab8fece076b49c9cf2cd2abdb706420c96a2
Vamsi-Pandu <vamsidevarapalli55@gmail.com>
2021-08-05 10:21:16 +0530

    response entity

--- developercommunityapp/src/main/java/com/cg/dca/entity/Response.java
+++ developercommunityapp/src/main/java/com/cg/dca/entity/Response.java
@@ -3,7 +3,19 @@ package com.cg.dca.entity;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="response")
+
 public class Response {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int respId;
 	private String answer;
 	private LocalDate respDate;
@@ -11,4 +23,62 @@ public class Response {
 	private int accuracy;	// Likes on Response increase accuracy
 	private Developer dev;
 	private Feed feed;
+	public Response(int respId, String answer, LocalDate respDate, LocalTime respTime, int accuracy, Developer dev,
+			Feed feed) {
+		super();
+		this.respId = respId;
+		this.answer = answer;
+		this.respDate = respDate;
+		this.respTime = respTime;
+		this.accuracy = accuracy;
+		this.dev = dev;
+		this.feed = feed;
+	}
+	public Response() {
+		super();
+	}
+	public int getRespId() {
+		return respId;
+	}
+	public void setRespId(int respId) {
+		this.respId = respId;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public LocalDate getRespDate() {
+		return respDate;
+	}
+	public void setRespDate(LocalDate respDate) {
+		this.respDate = respDate;
+	}
+	public LocalTime getRespTime() {
+		return respTime;
+	}
+	public void setRespTime(LocalTime respTime) {
+		this.respTime = respTime;
+	}
+	public int getAccuracy() {
+		return accuracy;
+	}
+	public void setAccuracy(int accuracy) {
+		this.accuracy = accuracy;
+	}
+	public Developer getDev() {
+		return dev;
+	}
+	public void setDev(Developer dev) {
+		this.dev = dev;
+	}
+	public Feed getFeed() {
+		return feed;
+	}
+	public void setFeed(Feed feed) {
+		this.feed = feed;
+	}
+
+
 }
