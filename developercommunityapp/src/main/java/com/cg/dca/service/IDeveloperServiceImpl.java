7ff97eb45ef916bb5a47ea7d01de3fc09dfe6a2b
Azmaths <47237313+Azmaths@users.noreply.github.com>
2021-08-05 10:24:57 +0530

    Added IDeveloper Service Implementation Class

---
+++ developercommunityapp/src/main/java/com/cg/dca/service/IDeveloperServiceImpl.java
@@ -0,0 +1,40 @@
+package com.cg.dca.service;
+
+import java.util.List;
+
+import com.cg.dca.entity.Developer;
+import com.cg.dca.exception.UnknownDeveloperException;
+
+public class IDeveloperServiceImpl  implements IDeveloperService {
+
+	@Override
+	public Developer addDeveloper(Developer dev) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Developer editDeveloper(Developer dev) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Developer statusUpdate(Developer dev) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Developer getDeveloper(int devId) throws UnknownDeveloperException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Developer> getAllDevelopers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
