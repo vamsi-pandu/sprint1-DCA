b1c7be3be7dd8f4a6b9aa1f916c06e9b7c945bbd
Azmaths <47237313+Azmaths@users.noreply.github.com>
2021-08-06 13:44:51 +0530

    added service annotation in developer implementation class

--- developercommunityapp/src/main/java/com/cg/dca/service/IDeveloperService.java
+++ developercommunityapp/src/main/java/com/cg/dca/service/IDeveloperService.java
@@ -3,19 +3,21 @@ package com.cg.dca.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
 import com.cg.dca.entity.Developer;
 import com.cg.dca.exception.UnknownDeveloperException;
 
-
+@Service
 public interface IDeveloperService {
 
-Optional<Developer> addDeveloper(Developer dev);
+	Optional<Developer> addDeveloper(Developer dev);
 	
-Optional<Developer> editDeveloper(Developer dev);
+	Optional<Developer> editDeveloper(Developer dev);
 	
      String statusUpdate(Developer dev);	// Block/Unblock
 	
-Optional<Developer> getDeveloper(int devId) throws UnknownDeveloperException;
+     Optional<Developer> getDeveloper(int devId) throws UnknownDeveloperException;
 	
 	List<Developer> getAllDevelopers();
 }
