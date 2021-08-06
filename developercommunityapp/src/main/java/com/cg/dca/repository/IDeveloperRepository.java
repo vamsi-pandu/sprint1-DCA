9892b5b65500d8b92178a4bc80cfd064ef3e22f7
Azmaths <47237313+Azmaths@users.noreply.github.com>
2021-08-06 10:05:22 +0530

    Update IDeveloperRepository.java

--- developercommunityapp/src/main/java/com/cg/dca/repository/IDeveloperRepository.java
+++ developercommunityapp/src/main/java/com/cg/dca/repository/IDeveloperRepository.java
@@ -3,10 +3,11 @@ package com.cg.dca.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cg.dca.entity.Developer;
 import com.cg.dca.exception.UnknownDeveloperException;
-
+@Repository
 public interface IDeveloperRepository extends JpaRepository<Developer,Integer> {
 
 	Developer saveDeveloper(Developer dev);
