89584e2ba02666599b9c09c3f0f1f4ded91f4f43
shashudhar <shshidharshiva123@gmail.com>
2021-09-07 23:29:17 +0530

    s

--- developercummunityapplication/src/main/java/com/cg/dca/service/IUserServiceImpl.java
+++ developercummunityapplication/src/main/java/com/cg/dca/service/IUserServiceImpl.java
@@ -28,7 +28,7 @@ public class IUserServiceImpl implements IUserService{
 
 	@Override
 	public Optional<User> addUser(User user) {
-		//findById is the method to check the multiple responses by userId
+		//findById method is used to find the user id in the database
 		Optional<User> newUser = userRepo.findById(user.getUserId());
 
 		if(!newUser.isPresent()) {
